67819405f4f3bb38404c95361ceb4bc09087466d
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-18 02:05:19 +0545

    D2: Created collections,product page, navbar , search filter etc

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+    const submitHandler = (e)=>{
+        e.preventDefault();
+    }
+
+  return (
+    <div className='text-center'>
+    <p className="text-2xl font-medium text-gray-800">Subscribe now & get 20% off</p>
+    <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, maiores.
+    </p>
+    <form onSubmit={submitHandler} className='flex items-center w-full sm:1/2 gap-3 mx-auto my-6 border pl-3'>
+        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
+        <button type='submit' className='bg-red-500 text-white text-xs font-bold px-10 py-4 rounded'>SUBSCRIBE</button>
+    </form>
+
+    </div>
+  )
+}
+
+export default NewsletterBox
