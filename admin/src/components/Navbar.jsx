4fd04055afb604ffcd815bcffb9dee71a68bd61f
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-21 01:42:44 +0545

    created admin panel dashboard and get produts from admin in frontend

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
-    <div>
-        <img src={assets.logo }/>
-        <button>Logout</button>
+    <div className='flex items-center justify-between py-2'>
+        <img className='w-[max(10%, 80%)]' src={assets.logo }/>
+        <button onClick={()=> setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,15 @@ import { ShopContext } from '../context/ShopContext.jsx';
 const Navbar = () => {
 
   const [visible, setVisible] = useState(false);
-  const {setShowSearch, getCartCount} = useContext(ShopContext);
+  const {setShowSearch, getCartCount, navigate, token, setToken, setCartItems} = useContext(ShopContext);
+
+  const logout = ()=>{
+     navigate('/login')
+    localStorage.removeItem('token')
+    setToken('')
+    setCartItems({})
+   
+  }
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
      <Link to='/'><img src={assets.logo} className='w-36 '/></Link> 
@@ -31,14 +39,18 @@ const Navbar = () => {
      <div className="flex items-center gap-6">
       <img  onClick={()=> setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer'/>
       <div className="group relative">
-       <Link to='/login'><img src={assets.profile_icon} className='w-5 cursor-pointer'/></Link> 
-        <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+       <img onClick={()=> token ? null : navigate('/login')} src={assets.profile_icon} className='w-5 cursor-pointer'/>
+       {/* ----DropDown Menu--------- */}
+       {
+        token && 
+         <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
           <div className=" flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-500 rounded">
            <p className="cursor-pointer hover:text-black">My Profile</p>
-           <p className="cursor-pointer hover:text-black">Oders</p>
-           <p className="cursor-pointer hover:text-black">Logout</p>
+           <p onClick={()=> navigate('/orders')} className="cursor-pointer hover:text-black">Oders</p>
+           <p onClick={logout} className="cursor-pointer hover:text-black">Logout</p>
           </div>
         </div>
+       }
 
       </div>
       <Link to='/cart' className='relative'>
